5018c40455817d42eb0acc5e919ac8b1f4b7833c
Mason Austin <mason47austin@gmail.com>
2023-06-03 12:58:16 -0500

    created the viewer for vocab cards, and created cleardom function

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,7 +1,12 @@
 import domBuilder from '../shared/domBuilder';
+import getVocabCards from '../api/vocabData';
+import { showVocabCards } from '../pages/vocabCards';
+import logoutButton from '../components/buttons/logoutButton';
 
 const startApp = (user) => {
   domBuilder(user);
+  logoutButton();
+  getVocabCards().then((cards) => showVocabCards(cards));
 };
 
 export default startApp;
