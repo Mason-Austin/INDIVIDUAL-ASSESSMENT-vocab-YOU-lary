6c15a78df38c75c0e1991fa27ed40866a7251656
Mason Austin <mason47austin@gmail.com>
2023-06-03 13:18:08 -0500

    created basic navbar

--- utils/startApp.js
+++ utils/startApp.js
@@ -2,9 +2,11 @@ import domBuilder from '../shared/domBuilder';
 import getVocabCards from '../api/vocabData';
 import { showVocabCards } from '../pages/vocabCards';
 import logoutButton from '../components/buttons/logoutButton';
+import navBar from '../shared/navBar';
 
 const startApp = (user) => {
   domBuilder(user);
+  navBar();
   logoutButton();
   getVocabCards().then((cards) => showVocabCards(cards));
 };
