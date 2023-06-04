5e3897c55435761f122dfbdeb92a1e73634a1545
Mason Austin <mason47austin@gmail.com>
2023-06-04 16:59:56 -0500

    created get singleVocabCard

--- api/vocabData.js
+++ api/vocabData.js
@@ -56,6 +56,18 @@ const deleteVocabCard = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const getSingleVocabCard = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}vocab/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
-  getVocabCards, createVocabCard, updateVocabCard, deleteVocabCard
+  getVocabCards, createVocabCard, updateVocabCard, deleteVocabCard, getSingleVocabCard
 };
