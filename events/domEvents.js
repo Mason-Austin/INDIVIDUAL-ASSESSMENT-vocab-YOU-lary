054776079d6182ac2ca3297f099462a807171d03
Mason Austin <mason47austin@gmail.com>
2023-06-05 21:05:24 -0500

    created event listeners for filter btns

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,4 +1,6 @@
-import { deleteVocabCard, getSingleVocabCard, getVocabCards } from '../api/vocabData';
+import {
+  deleteVocabCard, filterVocabCards, getSingleVocabCard, getVocabCards
+} from '../api/vocabData';
 import addCardForm from '../components/form/addCardForm';
 import { showVocabCards } from '../pages/vocabCards';
 
@@ -15,6 +17,25 @@ const domEvents = (user) => {
       const [, firebaseKey] = (e.target.id.split('--'));
 
       getSingleVocabCard(firebaseKey).then((cardObj) => addCardForm(cardObj));
+    }
+    if (e.target.id === ('htmlBtn')) {
+      // eslint-disable-next-line quotes
+      filterVocabCards(user.uid, "HTML").then(showVocabCards);
+    }
+    if (e.target.id.includes('cssBtn')) {
+      // eslint-disable-next-line quotes
+      filterVocabCards(user.uid, "CSS").then(showVocabCards);
+    }
+    if (e.target.id === ('javaBtn')) {
+      // eslint-disable-next-line quotes
+      filterVocabCards(user.uid, "JavaScript").then(showVocabCards);
+    }
+    if (e.target.id === ('pythonBtn')) {
+      // eslint-disable-next-line quotes
+      filterVocabCards(user.uid, "Python").then(showVocabCards);
+    }
+    if (e.target.id === ('clearBtn')) {
+      getVocabCards(user.uid).then(showVocabCards);
     }
   });
 };
