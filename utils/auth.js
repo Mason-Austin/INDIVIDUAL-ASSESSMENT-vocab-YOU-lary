e9a0c52d1632a19e67756a46ef8033045cabbf02
Mason Austin <mason47austin@gmail.com>
2023-06-10 02:17:35 -0500

    final css touches

--- utils/auth.js
+++ utils/auth.js
@@ -1,12 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import clearDom from './clearDom';
 
 const signIn = () => {
+  document.querySelector('#login-form-container').innerHTML = '';
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
 };
 
 const signOut = () => {
+  clearDom();
+  document.querySelector('#navBar').innerHTML = '';
   firebase.auth().signOut();
 };
 
